fa6ad33c97f9082f6d7421236215edccfe203579
Tj Thouhid <tjthouhid@tjs-imac.lan>
2021-09-01 13:16:09 +0600

    initialize plugin

---
+++ README.md
@@ -0,0 +1,1 @@
+# wp-vendor-admin
---
+++ wp-vendor-admin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *
+ * @link              https://www.tjthouhid.me
+ * @since             1.0.0
+ * @package           WP_VENDOR_ADMIN
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP VENDOR ADMIN
+ * Plugin URI:        https://github.com/tjthouhid/wp-vendor-admin
+ * Description:       You Can Use Custom Amount to pay via tera wallet.also added custom api endpoint for this.
+ * Version:           1.0.0
+ * Author:            Tj Thouhid
+ * Author URI:        https://www.tjthouhid.me
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wp-vendor-admin
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'WP_VENDOR_ADMIN', '1.0.0' );
